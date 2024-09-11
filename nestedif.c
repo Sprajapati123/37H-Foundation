ddafe9ebb1ec8d4194e696d334cd63bbe083c198
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-11 11:52:26 +0545

    nested if

---
+++ nestedif.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+   int a,b,c,highest;
+
+   printf("Enter first number:"),
+   scanf("%d",&a);
+   printf("Enter second number:"),  
+   scanf("%d",&b);
+   printf("Enter third number:"),  
+   scanf("%d",&c);
+
+   if(a>b){
+        if(a>c){
+            highest = a;
+        }else{
+            highest = c;
+        }
+   }else{
+     if(b>c){
+        highest = b;
+     }else{
+        highest = c;
+     }
+   }
+
+   printf("The highest number is %d",highest);
+
+}

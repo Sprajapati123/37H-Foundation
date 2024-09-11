04bda063e42c18e3f0af71c6e9b1c28072a27969
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-11 11:21:33 +0545

    if else

---
+++ control.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+    /*
+    Decision control statements
+    if
+    if else
+    else if ladder
+    nested if
+    switch
+    */
+
+
+    int a;
+
+    printf("Enter any number: ");
+    scanf("%d",&a);
+
+//10
+/*
+if(condition){
+
+}
+*/
+    if(a%2 == 0){
+        printf("The number is even");
+    }else{
+        printf("The number is odd");
+    }
+
+
+
+}
